1f8d98a75e6709f7d71688a5f70ec46b7704d0b3
d3nt <29033313+d3n7@users.noreply.github.com>
2023-11-25 15:25:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ import base64
 
 st.set_page_config(page_title='GPT-4 Vision', page_icon='👁️')
 
-if 'initialized' not in st.session_state:
+if 'history' not in st.session_state:
     st.session_state['history'] = [{'role': 'system', 'content': ''}]
     st.session_state['cost'] = 0.0
-    st.session_state['initialized'] = True
+    st.session_state['counters'] = [0, 1]
 
 st.markdown('# GPT-4 Vision Client')
 api_key = st.text_input('OpenAI API Key', '', type='password')
@@ -49,8 +49,8 @@ with chatTab:
             st.markdown('Assistant: ' + msg_content)
 
     # get user inputs
-    text_input = st.text_input('Prompt', '')
-    img_input = st.file_uploader('Images', accept_multiple_files=True)
+    text_input = st.text_input('Prompt', '', key=st.session_state['counters'][0])
+    img_input = st.file_uploader('Images', accept_multiple_files=True, key=st.session_state['counters'][1])
 
     # set up button layout
     st.markdown(
@@ -74,45 +74,49 @@ with chatTab:
     # send api request
     with cols[0]:
         if st.button('Send'):
-            if api_key:
-                if text_input or img_input:
-                    msg = {'role': 'user', 'content': []}
-                    if text_input:
-                        msg['content'].append({'type': 'text', 'text': text_input})
-                    for img in img_input:
-                        encoded_img = base64.b64encode(img.read()).decode('utf-8')
-                        msg['content'].append(
-                            {
-                                'type': 'image_url',
-                                'image_url': {
-                                    'url': f'data:image/jpeg;base64,{encoded_img}',
-                                    'detail': image_detail
-                                }
-                            }
-                        )
-                    st.session_state['history'].append(msg)
-                    history = (
-                        st.session_state['history']
-                        if st.session_state['history'][0]['content']
-                        else st.session_state['history'][1:]
-                    )
-                    client = OpenAI(api_key=api_key)
-                    response = client.chat.completions.create(
-                        model='gpt-4-vision-preview',
-                        temperature=temperature,
-                        max_tokens=max_tokens,
-                        messages=history
-                    )
-                    st.session_state['history'].append(
-                        {'role': 'assistant', 'content': response.choices[0].message.content}
-                    )
-                    st.session_state['cost'] += response.usage.prompt_tokens * 0.01 / 1000
-                    st.session_state['cost'] += response.usage.completion_tokens * 0.03 / 1000
-                    st.rerun()
-                else:
-                    st.warning('You can\'t just send nothing!')
-            else:
+            if not api_key:
                 st.warning('API Key required')
+                st.stop()
+            if not (text_input or img_input):
+                st.warning('You can\'t just send nothing!')
+                st.stop()
+            msg = {'role': 'user', 'content': []}
+            if text_input:
+                msg['content'].append({'type': 'text', 'text': text_input})
+            for img in img_input:
+                if img.name.split('.')[-1].lower() not in ['png', 'jpg', 'jpeg', 'gif', 'webp']:
+                    st.warning('Only .jpg, .png, .gif, or .webp are supported')
+                    st.stop()
+                encoded_img = base64.b64encode(img.read()).decode('utf-8')
+                msg['content'].append(
+                    {
+                        'type': 'image_url',
+                        'image_url': {
+                            'url': f'data:image/jpeg;base64,{encoded_img}',
+                            'detail': image_detail
+                        }
+                    }
+                )
+            st.session_state['history'].append(msg)
+            history = (
+                st.session_state['history']
+                if st.session_state['history'][0]['content']
+                else st.session_state['history'][1:]
+            )
+            client = OpenAI(api_key=api_key)
+            response = client.chat.completions.create(
+                model='gpt-4-vision-preview',
+                temperature=temperature,
+                max_tokens=max_tokens,
+                messages=history
+            )
+            st.session_state['history'].append(
+                {'role': 'assistant', 'content': response.choices[0].message.content}
+            )
+            st.session_state['cost'] += response.usage.prompt_tokens * 0.01 / 1000
+            st.session_state['cost'] += response.usage.completion_tokens * 0.03 / 1000
+            st.session_state['counters'] = [i+2 for i in st.session_state['counters']]
+            st.rerun()
 
     # clear chat history
     with cols[1]:
